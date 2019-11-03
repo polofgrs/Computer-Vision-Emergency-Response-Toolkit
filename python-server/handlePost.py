95dc11f2e0f84267a3412921e97049164023ac27
Paul Faugeras <paul.faugeras@supelec.fr>
2019-11-03 22:23:09 +0100

    simplified imports for adding algorithms much more easily

--- python-server/handlePost.py
+++ python-server/handlePost.py
@@ -1,16 +1,14 @@
 import cv2
 import numpy as np
 import base64
-import sys
-
-from Algorithms.RXDetector import RXD
-from Algorithms.DXDetector import DebrisDetect
-from Algorithms.AODNet import Dehaze
+import importlib
 
 def handlePost(algorithm, sourcePath, targetPath, parameters):
     if targetPath == 'response':
         image = data_uri_to_cv2_img(sourcePath)
-        resultImage = getattr(sys.modules[__name__], algorithm['pyFunction'])(image[1], parameters)
+        moduleName = 'Algorithms.' + algorithm['libName']
+        module = importlib.import_module(moduleName)
+        resultImage = getattr(module, algorithm['pyFunction'])(image[1], parameters)
         uri = cv2_img_to_data_uri(image[0], resultImage[0])
         resultDict = {
             'image': uri,
@@ -22,7 +20,6 @@ def handlePost(algorithm, sourcePath, targetPath, parameters):
 
 
 def data_uri_to_cv2_img(uri):
-    # imageList = uri.split(',')
     prefix = uri.split(',')[0]
     encoded_data = uri.split(',')[1]
     nparr = np.fromstring(base64.b64decode(encoded_data), np.uint8)
@@ -31,9 +28,6 @@ def data_uri_to_cv2_img(uri):
 
 
 def cv2_img_to_data_uri(prefix, resultImage):
-    # print(prefix)
     type = '.' + prefix.replace(';', '/').split('/')[1]
-    # print(type)
     string = base64.b64encode(cv2.imencode(type, resultImage)[1]).decode()
     return(string)
-    # return(string)
